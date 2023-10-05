3d55aec1a149af164c8be25383a1a4d1171434b2
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-05 07:32:50 -0700

    Updates

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Convert a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
