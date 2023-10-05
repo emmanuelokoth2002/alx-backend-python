0b426b6e8017d9c9f536fa7784eaa9af1d818d6c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-05 07:30:50 -0700

    Updates

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Return the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float as an integer.
+    """
+    return math.floor(n)
