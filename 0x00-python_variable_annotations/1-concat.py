67d8342209007fc160154820457de94de3eb34e3
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-05 07:28:32 -0700

    Updates

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+"""Concatenate two strings and return the result."""
 
 
 def concat(str1: str, str2: str) -> str:
     """
-    Concatenate two strings and return the result.
 
     Args:
         str1 (str): The first string.
