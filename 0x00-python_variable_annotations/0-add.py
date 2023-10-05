5cd96ee7d51c758904367fc5b18ed64aa438f6ba
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-05 07:24:47 -0700

    Updates

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+"""Add two floats and return their sum."""
+
 
 def add(a: float, b: float) -> float:
     """
-    Add two floats and return their sum.
-
     Args:
         a (float): The first float.
         b (float): The second float.
