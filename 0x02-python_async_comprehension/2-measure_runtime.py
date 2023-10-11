e1183ff8d55afc8b326a3945c80172fe34298c3e
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-11 09:15:26 -0700

    uPDATES

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,7 @@ from time import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Measure the total runtime of executing async_comprehension four times
