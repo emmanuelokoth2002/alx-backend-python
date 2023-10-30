cbde903abd43a61221f2c87064eb7bd50f8495a8
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-30 03:18:47 -0700

    Updates

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_result):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}' not found in nested map")
