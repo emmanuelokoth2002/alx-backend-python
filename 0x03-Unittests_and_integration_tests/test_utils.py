7b69f5d4c8c148ad9c8bd17fc461815040cb9edd
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-30 03:35:01 -0700

    Updates

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
 """
+Test cases for utils.access_nested_map
+Test cases for utils.access_nested_map
 Test cases for utils.access_nested_map
 """
 
@@ -10,19 +11,27 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test case for the access_nested_map function.
+    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected_result)
+    def test_access_nested_map(self, nestedMap, path, expectedResult):
+        """
+        Test the access_nested_map function
+        """
+        self.assertEqual(access_nested_map(nestedMap, path), expectedResult)
+
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_key):
-        with self.assertRaises(KeyError) as context:
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for diff. inputs """
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), f"'{expected_key}' not found in nested map")
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
